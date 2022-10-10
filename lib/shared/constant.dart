58a1cc092efc86f5dc1074f3c8fe408922118b5b
Badhon100 <badhonx102@gmail.com>
2022-10-10 22:05:06 +0600

    firebase setup done

---
+++ lib/shared/constant.dart
@@ -0,0 +1,6 @@
+class Constants{
+  static String appId = "1:248417937055:web:707ee985140fd5100564aa";
+  static String apiKey = "AIzaSyC4yy5PM5qs04aaW8_eqX4znFeO8PEt8hU";
+  static String messagingSenderId = "248417937055";
+  static String projectId = "chatappflutter-1faf6";
+}
