2d9bdf07cb9f9af806066cd9608f9efc97696ba0
Badhon100 <badhonx102@gmail.com>
2022-10-12 17:06:51 +0600

    structuring folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp_firebase/pages/homepage.dart';
 import 'package:chatapp_firebase/shared/constant.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const HomePage(),
     );
   }
 }
