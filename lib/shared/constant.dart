ea167427c8e11eda8460a82f595bc0bde0313e37
Badhon100 <badhonx102@gmail.com>
2022-10-14 17:52:11 +0600

    screenshoot added

--- lib/shared/constant.dart
+++ lib/shared/constant.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/animation.dart';
 
 class Constants{
-  static String appName = "Chat app";
-  final primaryColor = const Color(0xFFee7b64);
+  static String appName = "Group chat";
+  final primaryColor = const Color(0xFF2e94ec);
   static String appId = "1:248417937055:web:707ee985140fd5100564aa";
   static String apiKey = "AIzaSyC4yy5PM5qs04aaW8_eqX4znFeO8PEt8hU";
   static String messagingSenderId = "248417937055";
